595841815570fcbe141c0dddb38c5994f26a44a7
asanku <aswinidevuz01@gmail.com>
2024-10-09 13:19:27 +0530

    coach profile completed front end

--- frontend/src/app/editprocoach/editprocoach.component.ts
+++ frontend/src/app/editprocoach/editprocoach.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-editprocoach',
@@ -8,5 +9,18 @@ import { Component } from '@angular/core';
   styleUrl: './editprocoach.component.css'
 })
 export class EditprocoachComponent {
+  constructor(private router: Router) {}
+
+  editProfile(){
+    this.router.navigate(['/editprocoach']);
+  }
+
+  athleteass(){
+    this.router.navigate(['/athleteass']);
+  }
+  onSubmit(){
+    this.router.navigate(['/athleteprof']);
+  }
+
 
 }
